64b1745efa37c60fefba35de67f7fc8b6d8ba496
ashley-abbott <22324335+ashley-abbott@users.noreply.github.com>
2023-01-27 16:58:47 +0000

    bulking out the deployment (#5)
    * bulking out the deployment
    
    * revert action
    
    * Updated README [automated action]
    
    ---------
    
    Co-authored-by: Ashley Abbott <ash.abbott@appsbroker.com>
    Co-authored-by: github-actions[bot] <github-actions[bot]@noreply.github.com>

--- k8s_deployment.tf
+++ k8s_deployment.tf
@@ -52,30 +52,105 @@ resource "kubernetes_deployment" "application" {
         }
 
         spec {
-          automount_service_account_token = lookup(spec.value, "automount_service_account_token" ,null)
-          dns_policy = lookup(spec.value, "dns_policy", null)
-          service_account_name = lookup(spec.value, "service_account_name", null)
-          node_selector = lookup(spec.value, "node_selector", null)
-          node_name = lookup(spec.value, "node_name", null)
+          automount_service_account_token = lookup(spec.value, "automount_service_account_token", null)
+          dns_policy                      = lookup(spec.value, "dns_policy", null)
+          service_account_name            = lookup(spec.value, "service_account_name", null)
+          node_selector                   = lookup(spec.value, "node_selector", null)
+          node_name                       = lookup(spec.value, "node_name", null)
 
           dynamic "init_container" {
             for_each = try(spec.value["podspec"].init_containers, {})
 
             content {
-              name = init_container.value["name"]
-              image = init_container.value["image"]
+              name              = init_container.value["name"]
+              image             = init_container.value["image"]
               image_pull_policy = lookup(init_container.value, "image_pull_policy", null)
-              args = lookup(init_container.value, "args", null)
+              args              = lookup(init_container.value, "args", null)
+
+              dynamic "env" {
+                for_each = try(init_container.value["env"], {})
+
+                content {
+                  name = lookup(env.value, "name")
+                }
+              }
+
+              dynamic "env_from" {
+                for_each = try(init_container.value["env_from"], {})
+
+                content {
+                }
+              }
             }
           }
           dynamic "container" {
-            for_each = spec.value["podspec"].containers
-            
+            for_each = try(spec.value["podspec"].containers, {})
+
             content {
-              name = try(container.value["name"], var.app_name)
-              image = container.value["image"]
+              args              = lookup(container.value, "args", null)
+              command           = lookup(container.value, "command", null)
+              name              = try(container.value["name"], var.app_name)
+              image             = container.value["image"]
               image_pull_policy = lookup(container.value, "image_pull_policy", null)
-              args = lookup(container.value, "args", null)
+
+              dynamic "liveness_probe" {
+                for_each = try(container.value["liveness_probe"], {})
+
+                content {
+
+                }
+              }
+
+              dynamic "readiness_probe" {
+                for_each = try(container.value["readiness_probe"], {})
+
+                content {
+
+                }
+              }
+
+              dynamic "resources" {
+                for_each = try(container.value["resources"], {})
+
+                content {
+
+                }
+              }
+
+              dynamic "env" {
+                for_each = try(container.value["env"], {})
+
+                content {
+                  name  = lookup(env.value, "name")
+                  value = lookup(env.value, "value")
+                }
+              }
+
+              dynamic "env_from" {
+                for_each = try(container.value["env_from"], {})
+
+                content {
+
+                }
+              }
+
+              dynamic "volume_mount" {
+                for_each = try(container.value["volume_mount"], {})
+                iterator = mount
+                content {
+                  name       = lookup(mount.value, "name")
+                  mount_path = lookup(mount.value, "mount_path")
+                }
+              }
+            }
+          }
+
+          dynamic "volume" {
+            for_each = try(spec.value["volumes"], {})
+
+            content {
+              name = lookup(volume.value, "name", null)
+              
             }
           }
         }
