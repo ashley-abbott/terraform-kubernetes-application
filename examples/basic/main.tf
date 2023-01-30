5b2ef905721e882606872371bceffb132b2405b8
Ashley Abbott <ash.abbott@appsbroker.com>
2023-01-30 10:16:30 +0000

    - expand examples - add dynamic for ingress

---
+++ examples/basic/main.tf
@@ -0,0 +1,13 @@
+module "my_k8s_application" {
+  source = "../../"
+  app_name = "nginx"
+  namespace = "dev1"
+
+  deployment_spec = [{
+    podspec = {containers = [{image = "nginx:latest"}]}
+  }]
+
+  service_spec = [{
+    ports = [{"port" = 80}]
+  }]
+}
