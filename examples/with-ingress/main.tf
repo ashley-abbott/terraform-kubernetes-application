43ae8730ff4f79b46d0d31b7b2f66d46066bf03b
Ashley Abbott <ash.abbott@appsbroker.com>
2023-01-30 17:13:49 +0000

    add in example with ingress

---
+++ examples/with-ingress/main.tf
@@ -0,0 +1,49 @@
+module "my_k8s_application" {
+  source    = "../../"
+  app_name  = "nginx"
+  namespace = "dev1"
+
+  deployment_spec = [{
+    podspec = { containers = [{ image = "nginx:latest" }] }
+  }]
+
+  service_spec = [{
+    ports = [{ "port" = 80 }]
+  }]
+
+  ingress_spec = [{
+    ingress_class_name = "nginx"
+    rules = [
+      {
+        host = "example.me"
+        paths = [
+          {
+            path = "/app1/*"
+            backend = [{
+              service_port = 8080
+              service_name = "my-service1"
+            }]
+          },
+          {
+            path = "/app2/*"
+            backend = [{
+              service_port = 8443
+              service_name = "my-service2"
+            }]
+          }
+        ]
+      },
+      {
+        host = "anotherexample.com"
+        paths = [{
+          path      = "/blog/*"
+          path_type = "Prefix"
+          backend = [{
+            service_port = 9000
+            service_name = "my-service"
+          }]
+        }]
+      }
+    ]
+  }]
+}
