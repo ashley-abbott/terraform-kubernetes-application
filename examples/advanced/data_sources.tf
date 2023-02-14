21d98ed260c43f347d040c21a6c40f9a8f97b03d
Ashley Abbott <ash.abbott@appsbroker.com>
2023-02-14 17:08:59 +0000

    remove redundant steps

--- examples/advanced/data_sources.tf
+++
@@ -1,6 +0,0 @@
-# data "google_client_config" "provider" {}
-
-# data "google_container_cluster" "dev-cluster" {
-#   name     = "development"
-#   location = "europe-west2"
-# }
--- examples/advanced/providers.tf
+++ examples/advanced/providers.tf
@@ -1,11 +1,3 @@
-# provider "kubernetes" {
-#   host  = "https://${data.google_container_cluster.dev-cluster.endpoint}"
-#   token = data.google_client_config.provider.access_token
-#   cluster_ca_certificate = base64decode(
-#     data.google_container_cluster.dev-cluster.master_auth[0].cluster_ca_certificate,
-#   )
-# }
-
 provider "kubernetes" {
   config_path = "~/.kube/config"
 }
