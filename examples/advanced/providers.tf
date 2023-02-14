c4a3f601b4827d47db142b021e32929deec007de
Ashley Abbott <ash.abbott@appsbroker.com>
2023-02-14 16:02:02 +0000

    doesn't like that

--- examples/advanced/providers.tf
+++ examples/advanced/providers.tf
@@ -1,7 +1,7 @@
-provider "kubernetes" {
-  host  = "https://${data.google_container_cluster.dev-cluster.endpoint}"
-  token = data.google_client_config.provider.access_token
-  cluster_ca_certificate = base64decode(
-    data.google_container_cluster.dev-cluster.master_auth[0].cluster_ca_certificate,
-  )
-}
+# provider "kubernetes" {
+#   host  = "https://${data.google_container_cluster.dev-cluster.endpoint}"
+#   token = data.google_client_config.provider.access_token
+#   cluster_ca_certificate = base64decode(
+#     data.google_container_cluster.dev-cluster.master_auth[0].cluster_ca_certificate,
+#   )
+# }
